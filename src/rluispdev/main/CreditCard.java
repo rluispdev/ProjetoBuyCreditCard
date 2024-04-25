b0d363adf2215001877a487bb6b5223fe039b4bb
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-04-25 10:08:59 -0300

    Criar construtor, gettrs da classe CreditCard.

--- src/rluispdev/main/CreditCard.java
+++ src/rluispdev/main/CreditCard.java
@@ -1,10 +1,27 @@
 package rluispdev.main;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CreditCard {
     private double limit;
     private double balance;
     private List<Buy> buys;
+
+    public CreditCard(double limit) {
+        this.limit = limit;
+        this.balance = limit;
+        this.buys = new ArrayList<>();
+    }
+
+    public double getLimit() {
+        return limit;
+    }
+    public double getBalance() {
+        return balance;
+    }
+    public List<Buy> getBuys() {
+        return buys;
+    }
 }
 
