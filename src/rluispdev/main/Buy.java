b821ad7ab2c7200d2296fc27b7d9c68a8c7499ab
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-04-25 10:15:15 -0300

    Criar construtor, gettrs da classe Buy e já criar um toString.

--- src/rluispdev/main/Buy.java
+++ src/rluispdev/main/Buy.java
@@ -1,4 +1,26 @@
 package rluispdev.main;
 
 public class Buy {
+
+    private String description;
+    private double value;
+
+    public Buy(String description, double value) {
+        this.description = description;
+        this.value = value;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return  "Compra: descrição = " + description +
+                "valor = " + value;
+    }
 }
