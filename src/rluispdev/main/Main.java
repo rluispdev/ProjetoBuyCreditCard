f7cd5e8ee323e43d534f3e8d1b1ef4e135a475b0
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-04-25 10:28:44 -0300

    Criar o limite do Cartão.

--- src/rluispdev/main/Main.java
+++ src/rluispdev/main/Main.java
@@ -1,7 +1,16 @@
 package rluispdev.main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner read = new Scanner(System.in);
+        System.out.println("Digite o limite do Cartão: ");
+        double limit = read.nextDouble();
+        CreditCard card = new CreditCard(limit);
+
+
 
 
 
